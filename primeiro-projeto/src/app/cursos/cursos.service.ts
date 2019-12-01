b3fe5d19313d7b989ed3fd073c513fc862892941
fabiodaemon <fabiodaemon@yahoo.com.br>
2019-12-01 19:19:17 -0300

    templates e services

---
+++ primeiro-projeto/src/app/cursos/cursos.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+@Injectable()
+export class CursosService {
+
+  HttpModule
+
+  constructor() { }
+
+  getCursos(){
+    return ['Java', 'Ext JS', 'Angular'];
+  }
+}
