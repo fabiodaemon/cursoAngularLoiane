8a4e81aae419c3011a89dc81c44ad623b014fa74
fabiodaemon <fabiodaemon@yahoo.com.br>
2019-12-06 22:50:42 -0300

    eventos e classes CSS (review)

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  // styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight{
+        background-color: yellow;
+        font-weight: bold;
+      }
+
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http://loiane.com';
   cursoAngular: boolean =  true;
   urlImage = 'http://lorempixel.com/400/200/nature/';
+
+  valorAtual: string = '';
+  valorSalvo: string;
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1;
@@ -18,6 +31,24 @@ export class DataBindingComponent implements OnInit {
   getCurtirCurso(){
     return true;
   }
+
+  botaoClicado(){
+    alert('botao clicado');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+
 
   constructor() { }
 
