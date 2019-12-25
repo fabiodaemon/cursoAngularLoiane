66394991dff86dd93d40953e6832cabffe1b8fe0
fabiodaemon <fabiodaemon@yahoo.com.br>
2019-12-25 11:48:56 -0300

    trabalhando com injeçoes de dependencias - melhor pratica

--- servicos/src/app/cursos/cursos.component.ts
+++ servicos/src/app/cursos/cursos.component.ts
@@ -10,14 +10,19 @@ import { CursosService } from './cursos.service'
 export class CursosComponent implements OnInit {
 
   cursos: string[] = [];
-  cursosService: CursosService;
+  // cursosService: CursosService;
 
-  constructor() { 
-    this.cursosService = new CursosService();
+  constructor(private _cursosService: CursosService) { 
+    // // this.cursosService = new CursosService();
+    // // nao é boa prática fazer o acima, pois está instanciando manualmente e 
+    // caso ela tenha tb um constructor (a classe servico) isso daria problemas
+
+    // this.cursosService = _cursosService;
+    // este é um exemplo sem ser classe privada, caso ela seja é necessário fazer o que está definido aqui
    }
 
   ngOnInit() {
-    this.cursos = this.cursosService.getCursos();
+    this.cursos = this._cursosService.getCursos();
   }
 
 }
