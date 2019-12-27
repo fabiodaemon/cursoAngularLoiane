692d70b9fb2686fe2bf46bacd8500a22da4fbfb6
fabiodaemon <fabiodaemon@yahoo.com.br>
2019-12-26 21:28:32 -0300

    injetando um servico dentro de outro

--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
+import { LogService } from './../shared/log.service';
+
 @Injectable()
 export class CursosService{
 
@@ -10,15 +12,17 @@ export class CursosService{
 
     private cursos: string[] = ['Angular','Java','Phonegap'];
 
-    constructor(){
+    constructor(private logService: LogService){
         console.log('CursosService');
     }
 
     getCursos(){
+        this.logService.consoleLog('Obtendo lista de cursos');
         return this.cursos;
     }
 
     addCurso(curso: string){
+        this.logService.consoleLog(`Criando novo curso ${curso}`);
         this.cursos.push(curso);
         this.emitirCursoCriado.emit(curso);
         CursosService.criouNovoCurso.emit(curso);
