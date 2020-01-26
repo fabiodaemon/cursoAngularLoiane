be3172141391dc40487c82fcddd7115a86e4970c
fabiodaemon <fabiodaemon@yahoo.com.br>
2020-01-26 11:26:49 -0300

    intro a rotas (defasado do YT e atualizado no code)

---
+++ aula-rotas/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: "login", component: LoginComponent},
+  {path: "cursos", component: CursosComponent},
+  {path: "home", component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
