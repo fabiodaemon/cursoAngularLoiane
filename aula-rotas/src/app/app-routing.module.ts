a28d8622fe08094383599929d0b779f1d7f418c4
fabiodaemon <fabiodaemon@yahoo.com.br>
2020-01-26 20:04:41 -0300

    aula 55 e 56 - rotas imperativas e extrair parametros (queryParams)

--- aula-rotas/src/app/app-routing.module.ts
+++ aula-rotas/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: "login", component: LoginComponent},
+  {path: "home", component: HomeComponent},
   {path: "cursos", component: CursosComponent},
-  {path: "home", component: HomeComponent}
+  {path: "curso/:id", component: CursoDetalheComponent},
+  {path: "naoEncontrado", component: CursoNaoEncontradoComponent}
 ];
 
 @NgModule({
--- aula-rotas/src/app/app.module.ts
+++ aula-rotas/src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
