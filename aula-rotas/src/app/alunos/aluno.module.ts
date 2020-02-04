a44051f7fa81b643acc0189ee6b512a243b15ca9
fabiodaemon <fabiodaemon@yahoo.com.br>
2020-02-03 21:55:44 -0300

    Rotas Filhas: desenvolvendo as telas (oegando lista com id com button)

--- aula-rotas/src/app/alunos/aluno.module.ts
+++ aula-rotas/src/app/alunos/aluno.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AlunosComponent } from './alunos.component';
 import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './aluno.routing.odule';
+import { AlunosService } from './alunos.service';
+
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         AlunosRoutingModule
     ],
     exports: [],
@@ -17,7 +21,9 @@ import { AlunosRoutingModule } from './aluno.routing.odule';
         AlunoFormComponent, 
         AlunoDetalheComponent
     ],
-    providers: [],
+    providers: [
+        AlunosService
+    ],
 })
 
 export class AlunosModule{};
